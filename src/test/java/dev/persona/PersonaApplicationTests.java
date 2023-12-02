ed206459a4ab5fd587c97cafd850f0a71a5b09df
kushtency <kushagra.strive@gmail.com>
2023-12-02 21:35:34 +0530

    added files

---
+++ src/test/java/dev/persona/PersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.persona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
